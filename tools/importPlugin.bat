2c68b1a2b8798eddd1e553c767eb3d1fb9faaa8c
HUGUET Louis <louis.huguet@ate.at.int>
2025-04-22 13:17:10 +0200

    suppression de fichier inutile

--- tools/importPlugin.bat
+++ tools/importPlugin.bat
@@ -1,1 +1,4 @@
-robocopy "C:\Users\louis.huguet\Travail\Prog\ddtm_generationrapport" "C:\Users\louis.huguet\Travail\Plugins\DDTM06_GenerationRapport" /E /XF journal.txt /XD .git
+set INPUT_PATH="C:\Users\louis.huguet\Travail\Prog\ddtm_generationrapport"
+set OUTPUT_PATH="C:\Users\louis.huguet\Travail\Plugins\DDTM06_GenerationRapport"
+
+robocopy %INPUT_PATH% %OUTPUT_PATH% /E /XF journal.txt /XD .git
