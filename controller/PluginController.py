04e8ee154328af561df9c8928131b470ea163455
HUGUET Louis <louis.huguet@ate.at.int>
2025-07-04 11:24:22 +0200

    fix chargement couche

--- controller/PluginController.py
+++ controller/PluginController.py
@@ -2,7 +2,7 @@ from .FormController import FormController
 from ..model.CoucheModel import CoucheModel
 
 import os
-from qgis.core import QgsProject # type: ignore
+from qgis.core import QgsProject, QgsVectorLayer # type: ignore
 
 class PluginController():
     def __init__(self, dialog, couche_model_inst, config_model_inst, rapport_controller_inst):
@@ -14,11 +14,11 @@ class PluginController():
     def initPlugin(self):
         
         # Récupération de la couche
-        emplacement_type_etendu = os.path.join(os.path.dirname(__file__), '..', 'couches')
-        nom_couche = "type_etendu.shp"
+        emplacement_type_etendu = os.path.join(os.path.dirname(__file__), '..', 'couches', 'type_etendu.shp')
+        nom_couche = "type_etendu"
         
         # Création de la couche
-        vlayer = self.coucheModel.getCoucheFromFile(emplacement_type_etendu, nom_couche)
+        vlayer = QgsVectorLayer(emplacement_type_etendu, nom_couche, "ogr")
         
         # Chargement de la couche dans le projet
         QgsProject.instance().addMapLayer(vlayer)
